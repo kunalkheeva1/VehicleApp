889926faea0d10b9532f1fbaadcf7de3642ad7b9
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2023-06-29 22:13:46 -0700

    Initial commit

---
+++ src/Vehicle.java
@@ -0,0 +1,12 @@
+public class Vehicle {
+    private String name;
+    private int currentSpeed;
+    private int currentDirection;
+
+    public Vehicle(String name) {
+        this.name = name;
+        this.currentDirection =0;
+        this.currentSpeed =0;
+    }
+    
+}
